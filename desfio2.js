ebcee8e47ff658220982a55388d54d37d90e65e5
Marcos Henke <marcos.henke@dponet.com.br>
2023-03-03 12:29:41 -0300

    finalizando desafio 2

--- desfio2.js
+++ desfio2.js
@@ -1,3 +1,35 @@
-const frase = ('Hello World');
+let frase = 'Hello Wendel!';
 
-console.log(frase);
+let fraseNoSpace = frase.replace(/\s+/g, '')
+
+let fraseMin = fraseNoSpace.toLocaleLowerCase()
+
+let separeteLetters = fraseMin.split('')
+
+const objLetters = {}   
+
+separeteLetters.forEach((char, i) => {
+    
+    if (!objLetters.hasOwnProperty(char)) {
+        objLetters[char] = 1
+    } else {
+        objLetters[char]++
+    }
+});
+
+let arrayValues = Object.values(objLetters)
+
+console.log(arrayValues)
+
+let maxValue = 0
+
+arrayValues.forEach(value => {
+    if (value >= maxValue) {
+        maxValue = value
+    }
+})
+
+console.log(maxValue)
+
+
+
