cdea151a7af077c6071feffc5fb0f3447a8d4fd8
marcoshenke <marcos.henke3@gmail.com>
2023-03-05 19:19:48 -0300

    terminado o desafio 2

--- desfio2.js
+++ desfio2.js
@@ -1,25 +1,22 @@
-let frase = 'Hello Wendel!';
+let frase = 'Congratulations Wendel, Congratulations Mantelli, Brazzzzzzil!';
 
-let fraseNoSpace = frase.replace(/\s+/g, '')
+let fraseNoSpace = frase.replace(/\s+/g, '') // deleta os espaços
 
-let fraseMin = fraseNoSpace.toLocaleLowerCase()
+let fraseMin = fraseNoSpace.toLocaleLowerCase() // transforma em letra minuscula
 
-let separeteLetters = fraseMin.split('')
+let separeteLetters = fraseMin.split('') // separa cada letra da frase
 
 const objLetters = {}   
 
 separeteLetters.forEach((char, i) => {
-    
+    //vai adicionar cada letra para dentro do objeto objLetters, e se repetir adiciona mais um no valor dela
     if (!objLetters.hasOwnProperty(char)) {
         objLetters[char] = 1
     } else {
         objLetters[char]++
     }
 });
-
 let arrayValues = Object.values(objLetters)
-
-console.log(arrayValues)
 
 let maxValue = 0
 
@@ -29,7 +26,15 @@ arrayValues.forEach(value => {
     }
 })
 
-console.log(maxValue)
+//iterando sobre o objeto e encontrando as letras que mais apareceram
+for (var value in objLetters) {
+    if (objLetters[value] === maxValue) {
+        console.log(`a letra ${value} aparece ${maxValue} vezes`)
+        console.log(value + ' = ' + objLetters[value])
+    }
+}
+
+
 
 
 
